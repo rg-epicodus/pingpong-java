f311d41bd0665e04abd65a998bc55b58b729ebe0
David Cuthell and Rich Garrick <david@>
2017-07-31 15:13:11 -0700

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import models.PingPong;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by Guest on 7/31/17.
+ */
+public class App {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("I'm a ping-pong application.");
+        System.out.println("Enter a number");
+        try {
+            String stringUserNumber = bufferedReader.readLine();
+            int intUserNumber = Integer.parseInt(stringUserNumber);
+            PingPong pingPong = new PingPong();
+            ArrayList<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+            System.out.println(pingPongResult);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/models/PingPong.java
@@ -0,0 +1,26 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Guest on 7/31/17.
+ */
+public class PingPong {
+
+    public ArrayList<Object> runPingPong(int countUpTo){
+        ArrayList<Object> result = new ArrayList<Object>();
+        for (int i = 1; i <= countUpTo; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                result.add("pingpong");
+            } else if (i % 3 == 0){
+                result.add("ping");
+            } else if (i % 5 == 0){
+                result.add("pong");
+            } else {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
